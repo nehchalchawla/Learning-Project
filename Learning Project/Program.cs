b11bdb6cf96a7b2c97b0a3a35c921090ba7abf1e
HP <HP@N4-LAPTOP>
2022-08-23 12:12:36 +0700

    iuuui4

--- Learning Project/Program.cs
+++ Learning Project/Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");;;;;;;;
+Console.WriteLine("Hello, World!");;;;;;;;
+Console.WriteLine("Hello, World!");;;;;;;;
+Console.WriteLine("Hello, World!");;;;;;;;
