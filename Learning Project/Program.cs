8e76d2b5c6fd315e1e1d9b6345ab3f28709097e4
HP <HP@N4-LAPTOP>
2022-08-25 11:13:37 +0700

    challenges

--- Learning Project/Program.cs
+++ Learning Project/Program.cs
@@ -1,8 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");;;;;;;;
-Console.WriteLine("Hello, World!");;;;;;;;
-Console.WriteLine("Hello, World!");;;;;;;;
-Console.WriteLine("Hello, World!");;;;;;;;
+﻿////Challenge 1 
+//string Name = "Nehchal"; 
+//Console.WriteLine("Hello!");
+////Pascal Case 
+
+////Challenge 3 
+//string Name = Console.ReadLine();
+//Console.WriteLine("Hello" + Name);
+
+////Challenge 4 
+//int X = 7;
+//int Y = 3;
+//Console.WriteLine(X + Y);
+
+////Challenge 5 
+//double X = 5;
+//double  Y = 2;
+//Console.WriteLine(X / Y);
+////double stores a decimal amount 
+////int = whole numbers 
+
+////Challenge 6 
+//double TemperatureC = 32.5;
+//double TemperatureF = TemperatureC * 1.8 + 32;
+//Console.WriteLine("TemperatureF");
+
+////Challenge 7 
+//Console.WriteLine("Please enter your age.");
+//string Input = Console.ReadLine();
+//int Age = Convert.ToInt32(Input);
+//if (Age > 18)
+//{
+//    Console.WriteLine("you are old");
+//}
+//else if (Age < 18)
+//{
+//    Console.WriteLine("you are young");
+//}
+
+////Challenge 8 
+//string Input1 = Console.ReadLine();
+//string Input2 = Console.ReadLine();
+
+//int Num1 = Convert.ToInt32(Input1);
+//int Num2 = Convert.ToInt32(Input2);
+//     //  && = AND ||= Or 
+//if ((Num1 > 0 && Num2 < 0) || (Num1< 0 && Num2 > 0))
+//{
+//    Console.WriteLine("TRUE");
+//}
+//else
+//{
+//    Console.WriteLine("FAlSE");
+//}
+
+////Challenge 9 
+//string Input1 = Console.ReadLine();
+//string Input2 = Console.ReadLine();
+
+//int Num1 = Convert.ToInt32(Input1);
+//int Num2 = Convert.ToInt32(Input2);
+
+//if ((Num1 >= -10 && Num1 <= 10) || (Num2 >= -10 && Num2 <= 10))
+
+//{
+//    Console.WriteLine("TRUE");
+//}
+//else
+//{
+//    Console.WriteLine("FALSE");
+//}
+
+////Challenge 10 
+////for count = 1 to 99
+//for (int count = 1; count < 100; count = count + 1)
+//{
+//    Console.WriteLine(count);
+//}
+
+////Challenge 11 
+//for (int count = 1; count <= 50; count = count + 2)
+//{
+//    if (count % 5 == 0)
+//    {
+//        Console.WriteLine(count);
+//    }
+//}
+
+////Challenge 12
+//for (int count = -300; count <= -30; count = count + 3)
+//{
+//        Console.WriteLine(count);
+//}
+
+////Challenge 13 
+//int Count = 1;
+
+//string Input = "";
+//while (Input != "7")
+//{
+//    Input = Console.ReadLine();
+//}
+
+////Challenge 14 
+
+//string FirstName = "";
+//string LastName = "";
+
+//while (!(FirstName == "Santa" || LastName != "Claus"
+//{
+//    FirstName = Console.ReadLine();
+//    LastName = Console.ReadLine(); 
+//}
